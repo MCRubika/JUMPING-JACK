29f66fa42642a8f04f20e35e7a83be5e065ef97a
Marianne <m.cetkovic@rubika-edu.com>
2021-05-13 03:37:06 +0200

    Dialog + Reactivation

---
+++ Assets/ReactivationPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactivationPlayer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //if (//Le blabla avec le fermier est = 0 alors)
+            PlayerController.pcInstance.RestaurePlayerController();
+        }
+    }
+}
