43b71aa010717f49fb4e9eb18b30e51973962e9e
Marianne <m.cetkovic@rubika-edu.com>
2021-05-15 20:09:24 +0200

    Wwise music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AK.Wwise.Event musique;
+
+    private void Start()
+    {
+        musique.Post(gameObject);
+    }
+
+}
