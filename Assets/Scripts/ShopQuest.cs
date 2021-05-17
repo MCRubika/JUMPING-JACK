87d43befd69f0a63c3c4403d72311ef4a879034f
Marianne <m.cetkovic@rubika-edu.com>
2021-05-17 05:27:36 +0200

    Adds & Fixs

--- Assets/Scripts/ShopQuest.cs
+++ Assets/Scripts/ShopQuest.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class ShopQuest : MonoBehaviour
 {
     public GameObject LegsPlayer;
+    public GameObject Rewards;
 
+    private void Start()
+    {
+        Rewards.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player" && LevelManager.instance.pieces >= 3)
         {
             LevelManager.instance.playerPrefab = LegsPlayer;
+            Rewards.SetActive(true);
         }
     }
 }
